d7723505993d2daaf8b67382a2d565bb6ed14a13
unknown <vallee.neutral@gmail.com>
2024-06-06 19:14:13 +0100

    'retraining'

--- test_.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "https://us-central1-storied-polymer-421713.cloudfunctions.net/predict"
-files = {"file": open("./0cd1ebaf-3975-4749-b625-50b7b0751c36___RS_Erly.B 7508.JPG", "rb")}
-response = requests.post(url, files=files)
-
-print(response.text)
