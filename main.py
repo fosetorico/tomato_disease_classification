3e4983e42028c3c531ae6af9ef01f29cc22c9031
unknown <vallee.neutral@gmail.com>
2024-12-02 18:43:48 +0000

    Prepare Base Model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tomato_disease_classification import logger
 from tomato_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from tomato_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -11,3 +12,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME = "Prepare base model"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = PrepareBaseModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======================x")
+except Exception as e:
+   logger.exception(e)
+   raise e
--- src/tomato_disease_classification/config/configuration.py
+++ src/tomato_disease_classification/config/configuration.py
@@ -1,6 +1,7 @@
 from tomato_disease_classification.constants import *
 from tomato_disease_classification.utils.common import read_yaml, create_directories
-from tomato_disease_classification.entity.config_entity import DataIngestionConfig
+from tomato_disease_classification.entity.config_entity import (DataIngestionConfig,
+                                                                PrepareBaseModelConfig)
 
 
 class ConfigurationManager:
@@ -14,8 +15,6 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
-
-    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -29,4 +28,22 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
       
