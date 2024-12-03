7f2e5ee133689131babc5cb4c07545df04f1025f
unknown <vallee.neutral@gmail.com>
2024-12-03 19:34:29 +0000

    Training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tomato_disease_classification import logger
 from tomato_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from tomato_disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from tomato_disease_classification.pipeline.stage_03_training import ModelTrainingPipeline 
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,18 @@ try:
    obj = PrepareBaseModelTrainingPipeline()
    obj.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======================x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+
+STAGE_NAME = "Training"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    logger.exception(e)
    raise e
